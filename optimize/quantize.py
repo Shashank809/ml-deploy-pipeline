5fe127e1129c75439a2bb50f74e38fae6b89fdb2
Shashank809 <naikshashankrnaik@gmail.com>
2026-04-17 12:09:29 +0530

    Add dynamic quantization for ONNX model

---
+++ optimize/quantize.py
@@ -0,0 +1,11 @@
+from onnxruntime.quantization import quantize_dynamic, QuantType
+
+print("Starting quantization...")
+
+quantize_dynamic(
+    "models/model.onnx",
+    "models/model_int8.onnx",
+    weight_type=QuantType.QInt8
+)
+
+print("Quantized model saved as models/model_int8.onnx")
