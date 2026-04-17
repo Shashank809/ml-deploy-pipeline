94709f8e3ae554eecf9fe4a9e06fc313d16ee675
Shashank809 <naikshashankrnaik@gmail.com>
2026-04-17 12:35:52 +0530

    Update quantize.py

--- optimize/quantize.py
+++ optimize/quantize.py
@@ -3,10 +3,9 @@ from onnxruntime.quantization import quantize_dynamic, QuantType
 print("Starting quantization...")
 
 quantize_dynamic(
-    "models/model.onnx",
-    "models/model_int8.onnx",
-    weight_type=QuantType.QInt8,
-    optimize_model=False 
+    model_input="models/model.onnx",
+    model_output="models/model_int8.onnx",
+    weight_type=QuantType.QInt8
 )
 
 print("Quantized model saved as models/model_int8.onnx")
